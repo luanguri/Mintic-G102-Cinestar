2b1ce04bd59c54babdbb9545bfb32f4121e40e8b
APUMMA <angeldelapuentem@gmail.com>
2022-11-09 12:32:47 -0500

    Enlace de TxtInput y button del Login

--- app/src/main/java/com/cinestar/MainActivity.java
+++ app/src/main/java/com/cinestar/MainActivity.java
@@ -2,13 +2,57 @@ package com.cinestar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Creación de objeto Register
+    TextInputEditText mTxtInpEmail;
+    TextInputEditText mTxtInpPassword;
+    Button mBtnLogin;
+    TextView mTxtRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Busqueda de ID
+        mTxtInpEmail=findViewById(R.id.TxtInpEmail);
+        mTxtInpPassword=findViewById(R.id.TxtInpPassword);
+        mBtnLogin=findViewById(R.id.BtnLogin);
+        mTxtRegister=findViewById(R.id.TxtRegister);
+
+
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        //Instancia del TxtRegister
+        mTxtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void login() {
+        String email = mTxtInpEmail.getText().toString();
+        String password = mTxtInpPassword.getText().toString();
+        Log.d("campo", "email: " + email);
+        Log.d("campo", "password: " + password);
     }
 }
--- app/src/main/java/com/cinestar/RegisterActivity.java
+++ app/src/main/java/com/cinestar/RegisterActivity.java
@@ -3,12 +3,24 @@ package com.cinestar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    //Creación de objetos================================================
+    /*TextInputEditText mTxtInpUserName;
+    TextInputEditText mTxtInpEmailR;
+    TextInputEditText mTxtInpPasswordR;
+    TextInputEditText mTxtInpConfirmPassword;
+    Button mBtnRegister;
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
     }
 }
