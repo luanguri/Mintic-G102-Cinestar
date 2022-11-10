aee4efbeff7535de0ae9baa1914c671c02b31af9
Phili23 <rmariacarmen78@gmail.com>
2022-11-10 07:12:20 -0500

    Seleccionar Genero

---
+++ app/src/main/java/com/cinestar/SeleccionarGeneroActivity.java
@@ -0,0 +1,16 @@
+package com.cinestar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SeleccionarGeneroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccionar_genero);
+
+}
+}
