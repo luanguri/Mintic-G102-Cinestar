3e10f9f944d3e1298665e64641f6f67a506f04c0
APUMMA <angeldelapuentem@gmail.com>
2022-11-18 22:27:59 -0500

    Implementación Sign In Google

--- app/src/main/java/com/cinestar/MainActivity.java
+++ app/src/main/java/com/cinestar/MainActivity.java
@@ -11,20 +11,38 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Creación de objeto Register
+    //Creación de objeto Register=============================================================================
     TextInputEditText mTxtInpEmail;
     TextInputEditText mTxtInpPassword;
     Button mBtnLogin;
+    SignInButton mbtnSingInGoogle;
     TextView mTxtRegister;
     FirebaseAuth mAuth;
+    FirebaseFirestore mFirestore;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final int REQUEST_CODE_GOOGLE = 1;
 
 
     @Override
@@ -32,14 +50,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Busqueda de ID
+        //Busqueda de ID======================================================================================
         mTxtInpEmail = findViewById(R.id.TxtInpEmail);
         mTxtInpPassword = findViewById(R.id.TxtInpPassword);
         mBtnLogin = findViewById(R.id.BtnLogin);
+        mbtnSingInGoogle = findViewById(R.id.btnSingInGoogle);
         mTxtRegister = findViewById(R.id.TxtRegister);
         mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
 
+        //CONFIGURE GOOGLE SIGN IN============================================================================
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        //LOGIN BUTTON========================================================================================
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,12 +74,101 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Instancia del TxtRegister
+        //SING IN GOOGLE BUTTON================================================================================
+        mbtnSingInGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInGoogle();
+            }
+        });
+
+        //Instancia del TxtRegister===========================================================================
         mTxtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+    // [START signin]
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REQUEST_CODE_GOOGLE);
+    }
+
+    /*// [START signin]
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+     */
+    // [START onactivityresult]
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == REQUEST_CODE_GOOGLE) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                //Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                //Log.w(TAG, "Google sign in failed", e);
+            }
+        }
+    }
+
+    // [START auth_with_google]
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            /*Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                             */
+                            String id = mAuth.getCurrentUser().getUid();
+                            checkUserExist(id);
+                        }else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG; "Error" "signInWithCredential:failure", task.getException());
+
+                        }
+                    }
+                });
+    }
+    private void checkUserExist(String id) {
+        mFirestore.collection("Users").document(id).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()){
+                    Intent intent = new Intent(MainActivity.this, SeleccionarGeneroActivity.class);
+                    startActivity(intent);
+                }else {
+                    String email = mAuth.getCurrentUser().getEmail();
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("email", email);
+                    mFirestore.collection("Users").document(id).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(MainActivity.this, SeleccionarGeneroActivity.class);
+                                startActivity(intent);
+                            }else {
+                                Toast.makeText(MainActivity.this, "Almacenamiento de usuario fallido", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
@@ -78,3 +194,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d("campo", "password: " + password);
     }
 }
+
+
+
